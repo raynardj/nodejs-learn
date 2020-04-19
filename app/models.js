56a61ba82c250f2356c04fb93952b55dfd89f493
raynardj <raynard@rasenn.com>
2020-04-19 23:16:11 +0800

    sql api test

--- app/models.js
+++ app/models.js
@@ -2,18 +2,42 @@ const {Sequelize,Model,DataTypes} = require('sequelize');
 
 const sequelize = new Sequelize('sqlite::memory:');
 
+class DBs extends Model{}
+DBs.init({
+    id:{type:DataTypes.INTEGER, autoIncrement:true,primaryKey:true},
+    name:{type:DataTypes.STRING},
+    conn_str:{type:DataTypes.TEXT},
+    md_doc:{type:DataTypes.TEXT, allowNull:true}
+}, {sequelize, modelName:"dbs"})
 
-class User extends Model {}
-User.init({
-    username:DataTypes.STRING,
-    birthday:DataTypes.DATE
-},{sequelize, modelName:"user"});
+class SqlApi extends Model {}
+SqlApi.init({
+    slug:{type:DataTypes.STRING,unique:"compositeIndex",primaryKey:true},
+    db_id:{
+        type:DataTypes.INTEGER,
+        references:{
+            model:DBs,
+            key:DBs.id,
+        }
+    },
+    sql:{type:DataTypes.TEXT},
+    is_temp:{type:DataTypes.BOOLEAN},
+    md_doc:{type:DataTypes.TEXT,allowNull:true}
+},{sequelize,modelName:"sqlapi"})
 
-sequelize.sync()
-    .then(()=> User.create({
-        username:'Lena',
-        birthday: new Date(1992,1,20)
-    }))
-    .then(jane => {
-        console.log(jane.toJSON())
+sequelize.sync();
+DBs.sync();
+SqlApi.sync();
+
+DBs.create({name:"hugedb1",conn_str:"abc/def/ghi"})
+    .then(db => {
+        SqlApi.create({
+            slug:"api1",
+            db_id:db.id,
+            sql:"select * from text",
+            is_temp:false, 
+        })
+        .then(api => {
+            console.log(api.toJSON());
+        })
     })
